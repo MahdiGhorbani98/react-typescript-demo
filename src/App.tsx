4acfd7e2fa590cfd8284e1fe192c61e73cfdd32b
oliver <mahdig502@gmail.com>
2022-06-27 22:06:38 +0430

    Restricting Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { List } from "./components/generics/List";
+import { RandomNumber } from "./components/restricting/RandomNumber";
 
 function App() {
-  return (
-    <>
-      {/* <List
-        items={["Blue", "Red", "Yellow"]}
-        onClick={(item) => {
-          console.log(item);
-        }}
-      />
-      <List
-        items={[1, 3, 6]}
-        onClick={(item) => {
-          console.log(item);
-        }}
-      /> */}
-      <List
-        items={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        onClick={(item) => {
-          console.log(item);
-        }}
-      />
-    </>
-  );
+  return <RandomNumber value={11} isPositive />;
 }
 
 export default App;
