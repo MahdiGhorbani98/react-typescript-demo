0c07a5a2f104cad95b7e96ffa3fe664d638572d1
oliver <mahdig502@gmail.com>
2022-06-28 16:42:00 +0430

    polymorphic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { CustomButton } from "./components/html/Button";
+import { Text } from "./components/polymorphic/Text";
 
 function App() {
   return (
-    <CustomButton
-      variant="primary"
-      onClick={() => {
-        console.log("clicked");
-      }}
-    >
-      Primary Button
-    </CustomButton>
+    <>
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
+    </>
   );
 }
 
